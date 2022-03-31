c4d6fd28b8222084e94954ef147d7bafaec41caa
Erasmo Jr <erasmo.junior@gmail.com>
2022-03-31 18:30:20 -0300

    senha: Gerar sequencia por data

--- src/main/java/io/red/playground/repositories/SenhaRepository.java
+++ src/main/java/io/red/playground/repositories/SenhaRepository.java
@@ -8,7 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SenhaRepository extends JpaRepository<Senha, Long> {
 
-    @Query()
-    Senha findMax();
-
 }
--- src/main/java/io/red/playground/service/Service.java
+++ src/main/java/io/red/playground/service/Service.java
@@ -4,6 +4,7 @@ package io.red.playground.service;
 import io.red.playground.model.Senha;
 import io.red.playground.repositories.SenhaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 @org.springframework.stereotype.Service
@@ -15,15 +16,20 @@ public class Service {
         this.repository = repository;
     }
 
-    public Senha gerarSenha() {
-        Senha novaSenha = new Senha();
-        final var ultimaSenha = repository.findMax();
-        System.out.println(ultimaSenha);
-        repository.save(novaSenha);
-        return novaSenha;
+    public Date generateSequenceByDate(Date date) {
+        List<Senha> senhas = repository.findByData(date);
+        if (senhas.isEmpty()) {
+            Senha senha = new Senha();
+            senha.setData(date);
+            senha.setNumero(1);
+            repository.save(senha);
+            return date;
+        } else {
+            Senha senha = senhas.get(0);
+            senha.setNumero(senha.getNumero() + 1);
+            repository.save(senha);
+            return senha.getData();
+        }
     }
 
-    public List<Senha> recuperarTodas() {
-        return repository.findAll();
-    }
 }
