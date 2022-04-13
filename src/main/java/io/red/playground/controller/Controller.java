bf63175c00206c9c28a49007e456ed12afbbf953
Erasmo Junior <erasmo.cjunior@gmail.com>
2022-04-13 15:36:52 -0300

    teste comparacao de horas

--- src/main/java/io/red/playground/controller/Controller.java
+++ src/main/java/io/red/playground/controller/Controller.java
@@ -3,12 +3,11 @@ package io.red.playground.controller;
 import io.red.playground.dto.SenhaDTO;
 import io.red.playground.model.Senha;
 import io.red.playground.service.SenhaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -16,6 +15,8 @@ import java.util.List;
 public class Controller {
 
     SenhaService senhaService;
+
+    private final Logger log = LoggerFactory.getLogger(Controller.class);
 
     public Controller(SenhaService senhaService) {
         this.senhaService = senhaService;
@@ -27,13 +28,28 @@ public class Controller {
 //    }
 
     @PostMapping("/agendar/")
-    public Senha agendar(@RequestBody SenhaDTO senhaDTO){
+    public Senha agendar(@RequestBody SenhaDTO senhaDTO) {
         return senhaService.agendar(senhaDTO);
+    }
+
+    @PostMapping("/comparar")
+    public void compararHoras() {
+        LocalTime horaEntrada = LocalTime.of(8, 0, 0);
+        LocalTime inputHora = LocalTime.of(8, 30, 0);
+        boolean ehAntes = inputHora.isBefore(horaEntrada);
+        boolean ate30MinutosDepois = inputHora.isAfter(horaEntrada.plusMinutes(30L));
+        String trintaMinutos = horaEntrada.plusMinutes(30L).toString();
+        log.info("Hora Entrada: {}", horaEntrada);
+        log.info("Hora Input: {}", inputHora);
+        log.info("Entrou antes ? {}", ehAntes);
+        log.info("Entrou 30min depois ? {}", ate30MinutosDepois);
+        log.info("Trinta minutos? {}", trintaMinutos);
+
     }
 
 
     @GetMapping
-    public List<Senha> recuperarSenhas(){
+    public List<Senha> recuperarSenhas() {
         return senhaService.recuperarTodas();
     }
 
